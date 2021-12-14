7d97d4361ab575c353542da460cf57ed1136e65b
MrBacchus11 <96079683+MrBacchus11@users.noreply.github.com>
2021-12-14 15:48:06 -0600

    Update openGUI.java

--- src/main/java/asperienranks/asperienranks/GUIManagers/openGUI.java
+++ src/main/java/asperienranks/asperienranks/GUIManagers/openGUI.java
@@ -9,7 +9,7 @@ import org.bukkit.inventory.ItemStack;
 
 public class openGUI implements Listener {
     public static void SetupGUI(Player player) {
-        Inventory gui = Bukkit.createInventory(player, 36, ChatColor.YELLOW + "Rank");
+        Inventory gui = Bukkit.createInventory(player, 36, ChatColor.LIGHT_PINK + "" + ChatColor.BOLD + "Asperien Ranks");
 
         ItemStack placeholder = ItemStackManager.PlaceHolder();
         ItemStack currentRank = ItemStackManager.CurrentRank(player);
